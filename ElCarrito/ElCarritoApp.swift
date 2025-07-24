4976ce67a2d20f7797bc16d701baa61fc0ef0d6c
Dalvin Segura <hello@dalvin.site>
2025-07-24 19:36:07 -0400

    Initial Commit

---
+++ ElCarrito/ElCarritoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElCarritoApp.swift
+//  ElCarrito
+//
+//  Created by Dalvin Segura on 24/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ElCarritoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
