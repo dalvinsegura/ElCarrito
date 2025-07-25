e5dd8d67077d06000b6149e44bfe5a412a8d48ef
Dalvin Segura <hello@dalvin.site>
2025-07-25 12:10:36 -0400

    feat: Initialize project with models, view structure, and store
    - Set up initial project using Xcode template
    - Define ShoppingList and ShoppingItem models
    - Create ShoppingStore ViewModel
    - Organize project folder structure and add placeholder view files

---
+++ ElCarrito/Models/ShoppingList.swift
@@ -0,0 +1,8 @@
+//
+//  ShoppingList.swift
+//  ElCarrito
+//
+//  Created by Dalvin Segura on 24/7/25.
+//
+
+import Foundation
---
+++ ElCarrito/Views/Lists/AllListsView.swift
@@ -0,0 +1,18 @@
+//
+//  AllListsView.swift
+//  ElCarrito
+//
+//  Created by Dalvin Segura on 25/7/25.
+//
+
+import SwiftUI
+
+struct AllListsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllListsView()
+}
